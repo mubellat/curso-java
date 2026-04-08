bd966079ca587ef8d800900da5a65d72d79ec22b
Murilo Santana <ssz.mu04@gmail.com>
2026-04-08 17:38:26 -0300

    3 aulas

---
+++ training/src/training/Test3.java
@@ -0,0 +1,10 @@
+package training;
+
+public class Test3 {
+	public static void main(String[] args) {
+		double peso = 100.00;
+		System.out.printf("Meu nome é Jurandir e eu tenho %.2f quilos!! \n", peso);
+		System.out.println("###  TESTE OK !!!  ###");
+		System.out.println("###  PAGA A COCAAAAAAAA :)  ###");
+	}
+}
